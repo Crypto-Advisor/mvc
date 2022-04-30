44ca942faa4a5fb7b841c022d991926598eea91b
Adrien <adrien@Adriens-MacBook-Pro.local>
2022-04-30 15:29:03 -0400

    initial commit

---
+++ starting-code/routes/expense.js
@@ -0,0 +1,25 @@
+// require express and it's router component
+const express = require('express');
+
+const router = express.Router();
+
+// require the middlewares and callback functions from the controller directory
+const { create, expenseById, read } = require('../controllers');
+
+
+// Create POST route to create an expense
+router.post('/expense/create', create);
+
+// Create PUT route to update an expense
+
+
+// Create DELETE route to remove an expense
+
+// Create GET route to read an expense
+router.get('/expense/:id', expenseById, read);
+
+// Create GET route to read a list of expenses
+
+// Create a callback to handle url params
+
+module.exports = router;
